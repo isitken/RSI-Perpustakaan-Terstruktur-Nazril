73b593c7980d9f2b8d7a2de3eab6534334ab7b3f
isitken <nazrilfarreli@gmail.com>
2025-11-12 19:59:03 +0700

    Initial commit untuk upload proyek perpustakaan

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="id">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Perpustakaan</title>
+  <style>
+    body {
+      font-family: Arial, sans-serif;
+      text-align: center;
+      margin-top: 50px;
+    }
+
+    a {
+      padding: 10px;
+      background-color: #4CAF50;
+      color: white;
+      text-decoration: none;
+      border-radius: 5px;
+      margin: 10px;
+      display: inline-block;
+    }
+
+    a:hover {
+      background-color: #45a049;
+    }
+  </style>
+</head>
+
+<body>
+  <h1>Selamat Datang di Perpustakaan Nazril</h1>
+  <p>Nazril Farrel Insava/245150401111038</p>
+  <p>Monggo Langsung Dipilih mau ngapain</p>
+  <div>
+    <a href="cari.php">Cari Buku</a><br>
+    <a href="pinjam/pinjam.php?fitur=read">Lihat Keranjang</a><br>
+    <a href="pinjam/pinjam.php?fitur=read">Peminjaman Buku</a><br>
+  </div>
+</body>
+
+</html>
